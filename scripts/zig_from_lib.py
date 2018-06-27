d6763a6340b11217f0a9dc6d11c5813516fd25ce
Nick Hoffman <malware.kitten@gmail.com>
2018-06-27 15:26:58 -0400

    updating with sdb

--- scripts/zig_from_lib.py
+++ scripts/zig_from_lib.py
@@ -40,11 +40,19 @@ def recursive_all_files(directory, ext_filter=None):
             ret.append(f)
     return ret
 
-def generate_zigs(f):
+def generate_zigs_json(f):
     r2p = r2pipe.open(f)
     #analyze and generate zigs
     r2p.cmd('aaa; zg')
     zigs = r2p.cmdj('zj')
+    r2p.quit()
+    return zigs
+
+def generate_zigs_sdb(f, output):
+    r2p = r2pipe.open(f)
+    #analyze and generate zigs
+    r2p.cmd('aaa; zg')
+    zigs = r2p.cmdj('zos %s' % output)
     r2p.quit()
     return zigs
 
@@ -62,7 +70,8 @@ def dedup(zignatures):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate R2 Zignatures from .lib files')
     parser.add_argument("-f", "--file", required=True, help=".lib file to use")
-    parser.add_argument("-o", "--output", required=True, help="output json")
+    parser.add_argument("-o", "--output", required=True, help="output filename")
+    parser.add_argument("-s", "--sdb", action='store_true', help="store as sdb files")
     args = parser.parse_args()
 
     with open(args.file,'rb') as fp:
@@ -73,14 +82,22 @@ if __name__ == '__main__':
         output = subprocess.check_output(command)
         results = []
         for f in recursive_all_files(target_path, 'obj'):
-            json_items = generate_zigs(f)
-            for zigs in json_items:
-                results.append(zigs)
+            if args.sdb:
+                generate_zigs_sdb(f, args.output)
+            else:
+                json_items = generate_zigs_json(f)
+                for zigs in json_items:
+                    results.append(zigs)
     else:
         print("File magic does not match, check to make sure this is a .lib file")
 
+    #cleanup
     shutil.rmtree(target_path)
-    uniq_results = dedup(results)
-    if len(uniq_results) > 0:
-        with open(args.output, 'w') as fp:
-            fp.write(pformat(uniq_results))
+    if args.sdb:
+        #no output needed, file was written in r2
+        pass
+    else:
+        uniq_results = dedup(results)
+        if len(uniq_results) > 0:
+            with open(args.output, 'w') as fp:
+                fp.write(pformat(uniq_results))
